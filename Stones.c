77ccc01e849bb822e857b19190126586408660ef
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2024-05-26 23:52:21 +0300

    Add files via upload

--- Stones.c
+++ Stones.c
@@ -41,39 +41,40 @@ char* CombitateColor(HeapS* a, HeapS* b)
    return a->Color_of_stones;
 }
 
-void Make_color(HeapS* heap_, char* color_)
+int Make_color(HeapS* heap_, char* color_)
 {
    int color = fromChartoInt(color_);
    switch (color)
    {
    case(0):
       heap_->Color_of_stones = "black";
-      break;
+      return 0;
    case(255255255):
       heap_->Color_of_stones = "white";
-      break;
+      return 0;
    case(128128128):
       heap_->Color_of_stones = "gray";
-      break;
+      return 0;
    case(128000128):
       heap_->Color_of_stones = "purple";
-      break;
+      return 0;
    case(255000000):
       heap_->Color_of_stones = "red";
-      break;
+      return 0;
    case(255255000):
       heap_->Color_of_stones = "yellow";
-      break;
+      return 0;
    case(128000):
       heap_->Color_of_stones = "green";
-      break;
+      return 0;
    case(255):
       heap_->Color_of_stones = "blue";
-      break;
+      return 0;
    default:
       printf("I dont know what is the color");
-      break;
+      return NULL;
    }
+   
 }
 
 void Operation(HeapS* heap_, char operator)
@@ -126,7 +127,7 @@ struct Data_Base
    size_t size;
 }; 
 
-HeapS* Make_heap(unsigned int number_, int operation1_, int operation2_)
+void Make_Element(Data_Base* a, unsigned int number_, int operation1_, int operation2_)
 {
    HeapS* heap = (HeapS*)malloc(sizeof(HeapS));
    if (heap == NULL)
@@ -135,9 +136,38 @@ HeapS* Make_heap(unsigned int number_, int operation1_, int operation2_)
    heap->Number_of_stones = number_;
    heap->Operation_with_stones_plus = operation1_;
    heap->Operation_with_stones_multy = operation2_;
-   heap->Color_of_stones = NULL;
+   if (Make_color == NULL)
+      heap->Color_of_stones = NULL;
 
-   return heap;
+   a->Heap[a->size] = heap;
+   a->size++;
+}
+
+int Find_Element_number(Data_Base* a, int b)
+{
+   for(int i = 0; i < a->size; i++)
+      if (a->Heap[i]->Number_of_stones == b)
+      {
+         return i;
+      }
+   return NULL;
+}
+
+int Find_Element_color(Data_Base* a, char* b)
+{
+   for (int i = 0; i < a->size; i++)
+      if (a->Heap[i]->Color_of_stones[0] == b[0])
+      {
+         int flag = 0;
+         for (int j = 0; b[j] != '\0' && a->Heap[i]->Color_of_stones[j] != '\0'; j++)
+         {
+            if (a->Heap[i]->Color_of_stones[j] != b[j])
+               flag = 1;
+         }
+         if (flag == 0)
+            return i;
+      }
+   return NULL;
 }
 
 void Del_heap(HeapS* a)
@@ -146,5 +176,116 @@ void Del_heap(HeapS* a)
    free(a->Operation_with_stones_plus);
    free(a->Operation_with_stones_multy);
    free(a->Color_of_stones);
+   free(a);
+}
+
+void Del_Element(Data_Base* a, void* needdelete, int typedelete)
+{
+   int number_element;
+   switch (typedelete)
+   {
+   case(0):
+      for (int i = 0; i < a->size; i++)
+      {
+         number_element = Find_Element_number(a, (int)needdelete);
+         if (number_element == NULL)
+            return;
+         Del_heap(a->Heap[number_element]);
+         a->size--;
+         Move_Elements(a, number_element);
+      }
+      break;
+   case(1):
+      for (int i = 0; i < a->size; i++)
+      {
+         number_element = Find_Element(a, (char*)needdelete);
+         Del_heap(a->Heap[number_element]);
+         a->size--;
+         Move_Elements(a, number_element);
+      }
+      break;
+   default:
+      printf("Wrong data");
+      return -1;
+   }
+}
+
+void Move_Elements(Data_Base* a, int number_element)
+{
+   for (int i = number_element; i < (int)a->size; i++)
+   {
+      a->Heap[i] = a->Heap[i + 1];
+   }
+}
+
+void PrintDB(Data_Base* a)
+{
+   for (int i = 0; i < a->size; i++)
+   {
+      printf("%d   %d \n %s \n %d \n %d", i, a->Heap[i]->Number_of_stones, 
+         a->Heap[i]->Color_of_stones, a->Heap[i]->Operation_with_stones_plus, a->Heap[i]->Operation_with_stones_multy);
+   }
+}
+
+Data_Base* CreateDB()
+{
+   Data_Base* Base = (Data_Base*)malloc(sizeof(Data_Base));
+
+   if (Base == NULL)
+      return -1;
+
+   Base->Heap = malloc(20 * sizeof(HeapS));
+   if (Base->Heap == NULL)
+      return -1;
+
+   Base->size = 0;
+   return Base;
+}
+
+void SortDB(Data_Base* a)
+{
+   quickSort(a, 0, a->size);
+}
+
+void swap(HeapS* a, HeapS* b) 
+{
+   HeapS temp = *a;
+   *a = *b;
+   *b = temp;
+}
+
+int partition(HeapS* arr, int low, int high) 
+{
+   HeapS pivot = arr[high];
+   int i = low - 1;
+
+   for (int j = low; j <= high - 1; j++) {
+      if (Min(arr[j], pivot) > 0) {
+         i++;
+         swap(&arr[i], &arr[j]);
+      }
+   }
+   swap(&arr[i + 1], &arr[high]);
+   return (i + 1);
+}
+
+void quickSort(Data_Base* arr, int low, int high) 
+{
+   if (low < high) {
+      int p = partition(arr, low, high);
+
+      quickSort(arr, low, p - 1);
+      quickSort(arr, p + 1, high);
+   }
+}
+
+int Min(HeapS a, HeapS b)
+{
+   return a.Number_of_stones - b.Number_of_stones;
+}
+
+void DelDB(Data_Base* a)
+{
+   free(a->Heap);
    free(a);
 }
